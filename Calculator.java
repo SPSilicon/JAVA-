1cde110ff0fb69320414317da292b34ed84d6478
SPSilicon <44769598+SPSilicon@users.noreply.github.com>
2019-05-16 17:05:08 +0900

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionListener;
 import java.util.Stack;
 import javax.swing.*;
 
-public class Calculator extends JFrame implements ActionListener {
+public class Calculator extends JFrame{
 	private JPanel panel;
 	private JTextField display;
 	private JButton[] buttons;
@@ -31,7 +31,63 @@ public class Calculator extends JFrame implements ActionListener {
 					buttons[index].setForeground(Color.blue);
 				buttons[index].setBackground(Color.yellow);
 				panel.add(buttons[index]);
-				buttons[index].addActionListener(this);
+				buttons[index].addActionListener(new ActionListener() // 무명함수
+				{
+					@Override
+					public void actionPerformed(ActionEvent e)
+					{
+						String command = e.getActionCommand(); //버튼의 text가 command로
+						if(command.charAt(0) == 'B') //Backspace일때
+						{
+							if(!prev.isEmpty())
+								display.setText(prev.pop());
+						}
+						else if (command.charAt(0) == 'C') { //C 혹은 CE일때
+							startOfNumber = true;
+							result = 0;
+							operator = "=";
+							prev.clear();
+							display.setText("0.0");
+						} else if (command.charAt(0) >= '0' && command.charAt(0) <= '9'
+								&&!command.equals("1/x")|| command.equals(".")) { //숫자 또는 소수점 일때			
+							if (startOfNumber == true)	// 처음시작하는 숫자일경우 기존 display를 지우고 시작
+							{
+								prev.push(" ");
+								display.setText(command);				
+							}
+							else
+							{
+								prev.push(display.getText());
+								display.setText(display.getText() + command);
+							}
+							startOfNumber = false; //숫자가 입력됫으므로 false
+						} else {// CE,E,숫자,소수점도 아니므로 operator일때의 상황
+							if (startOfNumber) {	// 처음시작하는 문자가 operator일 경우
+								if (command.equals("-")) { // - 일때 display를 전부 지우고 -출력(음수입력을 의미)
+									display.setText(command);
+									startOfNumber = false;
+								} else
+									operator = command;
+							} else { // 숫자 다음에 operator가 오므로 계산을 수행합니다.
+								double x = Double.parseDouble(display.getText());
+								 if(command.equals("sqrt")||command.equals("1/x"))
+								 {
+									 operator = command;
+									 calculate(x);
+								 }
+								 else
+								 {
+									calculate(x);		//계산
+									operator = command;	//입력한 operator를 저장
+								 }
+								startOfNumber = true;
+								prev.clear();
+							}
+							
+						}
+						
+					}
+				});
 				index++;
 			}
 		}
@@ -41,52 +97,7 @@ public class Calculator extends JFrame implements ActionListener {
 		setVisible(true);
 		pack();
 	}
-	@Override
-	public void actionPerformed(ActionEvent e)
-	{
-		String command = e.getActionCommand(); //버튼의 text가 command로
-		if(command.charAt(0) == 'B') //Backspace일때
-		{
-			if(!prev.isEmpty())
-				display.setText(prev.pop());
-		}
-		else if (command.charAt(0) == 'C') { //C 혹은 CE일때
-			startOfNumber = true;
-			result = 0;
-			operator = "=";
-			prev.clear();
-			display.setText("0.0");
-		} else if (command.charAt(0) >= '0' && command.charAt(0) <= '9'
-				|| command.equals(".")) { //숫자 또는 소수점 일때			
-			if (startOfNumber == true)	// 처음시작하는 숫자일경우 기존 display를 지우고 시작
-			{
-				prev.push(" ");
-				display.setText(command);				
-			}
-			else
-			{
-				prev.push(display.getText());
-				display.setText(display.getText() + command);
-			}
-			startOfNumber = false; //숫자가 입력됫으므로 false
-		} else {// CE,E,숫자,소수점도 아니므로 operator일때의 상황
-			if (startOfNumber) {	// 처음시작하는 문자가 operator일 경우
-				if (command.equals("-")) { // - 일때 display를 전부 지우고 -출력(음수입력을 의미)
-					display.setText(command);
-					startOfNumber = false;
-				} else
-					operator = command;
-			} else { // 숫자 다음에 operator가 오므로 계산을 수행합니다.
-				double x = Double.parseDouble(display.getText());
-				calculate(x);		//계산
-				operator = command;	//입력한 operator를 저장
-				startOfNumber = true;
-				prev.clear();
-			}
-			
-		}
-		
-	}
+
 	private void calculate(double n) {
 		if (operator.equals("+"))
 			result += n;
@@ -98,7 +109,20 @@ public class Calculator extends JFrame implements ActionListener {
 			result /= n;
 		else if (operator.equals("="))
 			result = n;
+		else if (operator.equals("1/x"))
+			result = 1/n;
+		else if (operator.equals("-/+"))
+			result = -n;
+		else if (operator.equals("sqrt"))
+			result = Math.sqrt(n);
+		else if (operator.equals("%"))
+			result %= n; 
 		display.setText("" + result);
 	}
-
+	
+	public static void main(String[] args) 
+	{
+			Calculator cal = new Calculator();
+	}
 }
+
